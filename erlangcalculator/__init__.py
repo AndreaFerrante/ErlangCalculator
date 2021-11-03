25a8d9332e0e172f4a9e2ca22cd45460b3116fae
AndreaFerrante <andreaferrante1986@gmail.com>
2021-11-03 20:42:40 +0100

    Adding __init__ and example

--- erlangcalculator/__init__.py
+++ erlangcalculator/__init__.py
@@ -1,3 +1,6 @@
 from erlangcalculator.erlang import Erlang
+from erlangcalculator.erlang_example import (
+	erlang_example_printer
+)
 
 __all__ = ['erlangcalculator']
--- erlangcalculator/erlang_example.py
+++ erlangcalculator/erlang_example.py
@@ -1,28 +1,40 @@
-from . import Erlang
+#import sys
+#sys.path.append(".")
+
+from erlangcalculator.erlang import Erlang
 
 
-##################
-er      = Erlang()
-##################
+def erlang_example_printer():
+
+	##################
+	er      = Erlang()
+	##################
 
 
-n       = 23   # inforced of the number of agents...
-lambda_ = 170  # calls per hour...
-AHT     = 400  # duration in seconds of the answer call...
-t       = 22   # targeting answering time...
-ap      = 65   # average patience of clients...
+	n       = 23   # - inforced of the number of agents...
+	lambda_ = 170  # - calls per hour...
+	AHT     = 400  # - duration in seconds of the answer call...
+	t       = 22   # - targeting answering time...
+	ap      = 65   # - average patience of clients...
 
 
-erlang  = lambda_ * (AHT / 3600) # this is the erlang as unit measure...
-teta    = 1 / (ap / 3600)
-mu      = 1 / (AHT / 3600)
-ro      = lambda_  / (n * mu)
-x       = (n * mu) / teta
-y       = lambda_  / teta
-AR      = er.ErlangA(x, y, ro)
-PW      = ( er.GammaFunction(x, y, 100) * er.ErlangB(erlang, n) ) / (1 + (er.GammaFunction(x, y, 100) - 1) * er.ErlangB(erlang, n) )
+	erlang  = lambda_ * (AHT / 3600) # this is the erlang as unit measure...
+	teta    = 1 / (ap / 3600)
+	mu      = 1 / (AHT / 3600)
+	ro      = lambda_  / (n * mu)
+	x       = (n * mu) / teta
+	y       = lambda_  / teta
+	AR      = er.ErlangA(x, y, ro)
+	PW      = ( er.GammaFunction(x, y, 100) * er.ErlangB(erlang, n) ) / (1 + (er.GammaFunction(x, y, 100) - 1) * er.ErlangB(erlang, n) )
 
 
-print('Service Level ', er.GradeOfService(erlang, n, t, AHT))
-print('Abandoned Rate ', AR * PW)
-print('Answered immediately', 1 - er.ErlangC(erlang, n))
+	print('---------------------------------------------------------')
+	print('Service Level ', er.GradeOfService(erlang, n, t, AHT))
+	print('Abandoned Rate ', AR * PW)
+	print('Answered immediately', 1 - er.ErlangC(erlang, n))
+	print('---------------------------------------------------------')
+
+
+
+
+
